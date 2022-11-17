1c7235e37bce682894611b6e1260e21c1bbf804d
xpestana <unibasileawe@gmail.com>
2022-11-17 12:36:09 -0430

    change

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,18 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+
+                //Styles
                 'resources/sass/app.scss',
                 'resources/js/app.js',
                 'resources/css/app.css',
+                'resources/css/custom.css',
+                
+
+                //Components
                 'resources/js/pages/Maintain.js',
                 'resources/js/pages/Home.js',
+                
             ],
             refresh: true,
         }),
