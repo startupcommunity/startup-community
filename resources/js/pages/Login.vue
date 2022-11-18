7f1be0f13aef62afaef33b8c38e4199bc5188b00
josecarruyo <josecarruyo.1913@gmail.com>
2022-11-18 14:58:37 -0400

    login

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,8 +1,33 @@
  <template> 
- este es el login
+
+ <div class="container">
+        <div class="row justify-content-center align-items-center" style="height:100vh">
+            <div class="col-4 colcentrado">
+				
+
+                <div class="card">
+					
+                    <div class="card-body">
+						<h1 class="titulo">Bienvenidos a <br>startups</h1>
+						
+                        <form action="" autocomplete="off">
+
+							
+                            <div class="form-group">
+                                <input type="text" class="form-control" name="username">
+                            </div>
 
 
-
+                            <div class="form-group">
+                                <input type="password" class="form-control" name="password">
+                            </div>
+                            <button type="button" id="sendlogin" class="btn btn-primary ">Vamos</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
