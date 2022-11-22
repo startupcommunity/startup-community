81d139318f0c997c67c067a4a46c0bd40ef325ef
xpestana <unibasileawe@gmail.com>
2022-11-21 19:54:04 -0430

    custom.css

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 'resources/sass/app.scss',
                 'resources/js/app.js',
                 'resources/css/app.css',
+                'resources/css/custom.css',
             ],
             refresh: true,
         }),
