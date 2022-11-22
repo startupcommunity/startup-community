005917b0a8ff521e7b6c2e24a5dd222613a35eef
xpestana <unibasileawe@gmail.com>
2022-11-21 19:42:17 -0430

    act vite

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,6 @@ export default defineConfig({
                 'resources/sass/app.scss',
                 'resources/js/app.js',
                 'resources/css/app.css',
-                'resources/js/pages/Maintain.js',
-                'resources/js/pages/Home.js',
             ],
             refresh: true,
         }),
